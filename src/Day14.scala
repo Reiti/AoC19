52d9facf2c1d3206bc8a422df6d520a127961bb2
Michael Reitgruber <reitgruber.michael@gmail.com>
2019-12-15 13:11:28 +0100

    Day 14 done

--- src/Day14.scala
+++ src/Day14.scala
@@ -11,66 +11,32 @@ case class Reaction(component: Component, parts: List[Component])
 object Day14 extends App   {
   val reactions = Util.loadDay(14).split("\n").map(parse).toList
 
-  val input1 = """10 ORE => 10 A
-                 |1 ORE => 1 B
-                 |7 A, 1 B => 1 C
-                 |7 A, 1 C => 1 D
-                 |7 A, 1 D => 1 E
-                 |7 A, 1 E => 1 FUEL""".stripMargin.split("\n").map(parse).toList
-  val input2 = """9 ORE => 2 A
-                 |8 ORE => 3 B
-                 |7 ORE => 5 C
-                 |3 A, 4 B => 1 AB
-                 |5 B, 7 C => 1 BC
-                 |4 C, 1 A => 1 CA
-                 |2 AB, 3 BC, 4 CA => 1 FUEL""".stripMargin.split("\n").map(parse).toList
-  val input3 = """157 ORE => 5 NZVS
-                 |165 ORE => 6 DCFZ
-                 |44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-                 |12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-                 |179 ORE => 7 PSHF
-                 |177 ORE => 5 HKGWZ
-                 |7 DCFZ, 7 PSHF => 2 XJWVT
-                 |165 ORE => 2 GPVTF
-                 |3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""".stripMargin.split("\n").map(parse).toList
-  val input4 = """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
-                 |17 NVRVD, 3 JNWZP => 8 VPVL
-                 |53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
-                 |22 VJHF, 37 MNCFX => 5 FWMGM
-                 |139 ORE => 4 NVRVD
-                 |144 ORE => 7 JNWZP
-                 |5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
-                 |5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
-                 |145 ORE => 6 MNCFX
-                 |1 NVRVD => 8 CXFTF
-                 |1 VJHF, 6 MNCFX => 4 RFSQX
-                 |176 ORE => 6 VJHF""".stripMargin.split("\n").map(parse).toList
-  val input5 ="""171 ORE => 8 CNZTR
-                      |7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
-                      |114 ORE => 4 BHXH
-                      |14 VRPVC => 6 BMBT
-                      |6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-                      |6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-                      |15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-                      |13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-                      |5 BMBT => 4 WPTQ
-                      |189 ORE => 9 KTJDG
-                      |1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
-                      |12 VRPVC, 27 CNZTR => 2 XDBXC
-                      |15 KTJDG, 12 BHXH => 5 XCVML
-                      |3 BHXH, 2 VRPVC => 7 MZWV
-                      |121 ORE => 7 VRPVC
-                      |7 XCVML => 6 RJRHP
-                      |5 BHXH, 4 VRPVC => 5 LTCX""".stripMargin.strip.split("\n").map(parse).toList
+  //Part 1
+  println(calculateOreCost(reactions, 1))
 
+  //Part 2
+  println(binarySearch(reactions, 0L, 1000000000L))
 
+  @tailrec
+  def binarySearch(reactions: List[Reaction], low: Long, high: Long): Long = {
+    val check = low + (high - low) / 2L
+    val needed = calculateOreCost(reactions, check)
+    val neededPlusOne = calculateOreCost(reactions, check + 1)
+    if(needed <= 1000000000000L && neededPlusOne > 1000000000000L) {
+      check
+    } else if (needed <=  1000000000000L) {
+      binarySearch(reactions, check, high)
+    } else {
+      binarySearch(reactions, low, check)
+    }
+  }
 
-  println(calculateOreCost(input3, 2))
   def calculateOreCost(reactions: List[Reaction], amount: Long): Long = {
-    val ore = List((Node("ORE"), Set(Node("ORE"))))
+    val ore = List((Node("ORE"), Set[Node]()))
     val parsed = (ore ++ reactions.map(r => (Node(r.component.name), r.parts.map(x => Node(x.name)).toSet))).toMap
     val graph = Graph(parsed)
-    work(reactions, graph.topologicalSort.get.map(_.label).filter(_ != "ORE"), Map("FUEL" -> amount))
+    //Double reversal with distinct only leaves the last occurence of a node in the list
+    work(reactions, graph.topologicalSort.get.reverse.distinct.reverse.map(_.label).filter(_ != "ORE"), Map("FUEL" -> amount))
   }
 
   @tailrec
@@ -79,7 +45,7 @@ object Day14 extends App   {
       val r = reactions.find(r => r.component.name == x).get
       val rq = r.component.quantity
       val cq = cost(x)
-      val total = Math.ceil(cq.toDouble / rq.toDouble).toInt
+      val total =  Math.ceil(cq.toDouble / rq.toDouble).toLong
       work(reactions, xs, addCost(r.parts, cost, total).removed(x))
     case Nil =>
       cost("ORE")
