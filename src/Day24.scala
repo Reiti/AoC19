91c4758822a6a8d8c5cc88ab90eff19a4116e1bb
Michael Reitgruber <reitgruber.michael@gmail.com>
2019-12-24 13:34:21 +0100

    Day 24 slight cleanup

--- src/Day24.scala
+++ src/Day24.scala
@@ -24,6 +24,7 @@ object Day24 extends App {
   def evolveStructure(map: Map[Int, Array[String]]): Map[Int, Array[String]] = {
     map.map(l => (l._1, evolveLayer(map, l._1)))
   }
+
   def evolveLayer(map: Map[Int, Array[String]], layer: Int): Array[String] = {
     val res = map(layer).zipWithIndex map { case (line, y) =>
       line.zipWithIndex map { case (char, x) =>
@@ -48,92 +49,51 @@ object Day24 extends App {
         }
       }
     }
-
     res.map(_.mkString)
   }
 
   def countRecursiveNeighbors(map: Map[Int, Array[String]], level: Int, x: Int, y: Int): Int = {
+    val curr = map(level).map(_.lift).lift
+    val u = count(curr(y-1) flatMap {_(x)} getOrElse '.')
+    val d = count(curr(y+1) flatMap {_(x)} getOrElse '.')
+    val r = count(curr(y) flatMap {_(x+1)} getOrElse '.')
+    val l = count(curr(y) flatMap {_(x-1)} getOrElse '.')
+
     if(x == 0 && y == 4 && level > -100) {
       val outer = count(map(level-1)(2)(1)) + count(map(level-1)(3)(2))
-      val u = count(map(level)(y - 1)(x))
-      val r = count(map(level)(y)(x+1))
-
       outer + u + r
     } else if(x == 0 && y == 0 && level > -100) {
       val outer = count(map(level-1)(2)(1)) + count(map(level-1)(1)(2))
-      val r = count(map(level)(y)(x+1))
-      val d = count(map(level)(y+1)(x))
-
       outer + r + d
     } else if(x == 4 && y == 0 && level > -100) {
       val outer = count(map(level-1)(1)(2)) + count(map(level-1)(2)(3))
-      val l = count(map(level)(y)(x-1))
-      val d = count(map(level)(y+1)(x))
-
       outer + l + d
     } else if(x == 4 && y == 4 && level > -100) {
       val outer = count(map(level-1)(2)(3)) + count(map(level-1)(3)(2))
-      val l = count(map(level)(y)(x-1))
-      val u = count(map(level)(y-1)(x))
-
       outer + l + u
     } else if(x == 0 && level > -100) {
       val outer = count(map(level-1)(2)(1))
-      val u = if(y > 0) count(map(level)(y-1)(x)) else 0
-      val d = if(y < 4) count(map(level)(y+1)(x)) else 0
-      val r = count(map(level)(y)(x+1))
-
       outer + u + d + r
     } else if(x == 4 && level > -100) {
       val outer = count(map(level-1)(2)(3))
-      val u = if(y > 0) count(map(level)(y-1)(x)) else 0
-      val d = if(y < 4) count(map(level)(y+1)(x)) else 0
-      val l = count(map(level)(y)(x-1))
-
       outer + u + d + l
     } else if(y == 0 && level > -100) {
       val outer = count(map(level-1)(1)(2))
-      val l = if(x > 0) count(map(level)(y)(x-1)) else 0
-      val r = if(x < 4) count(map(level)(y)(x+1)) else 0
-      val d = count(map(level)(y+1)(x))
-
       outer + l + r + d
-
     } else if(y == 4 && level > -100) {
       val outer = count(map(level-1)(3)(2))
-      val l = if(x > 0) count(map(level)(y)(x-1)) else 0
-      val r = if(x < 4) count(map(level)(y)(x+1)) else 0
-      val u = count(map(level)(y-1)(x))
-
       outer + l + r + u
-
     } else if(x == 2 && y == 1 && level < 100) {
       val inner = map(level+1)(0).map(count).sum
-      val u = count(map(level)(y-1)(x))
-      val l = count(map(level)(y)(x-1))
-      val r = count(map(level)(y)(x+1))
-
       inner + u + l + r
     } else if(x == 1 && y == 2 && level < 100) {
       val inner = map(level+1).map(_.head).map(count).sum
-      val u = count(map(level)(y-1)(x))
-      val l = count(map(level)(y)(x-1))
-      val d = count(map(level)(y+1)(x))
-
       inner + u + l + d
     } else if(x == 3 && y == 2 && level < 100) {
       val inner = map(level+1).map(_(4)).map(count).sum
-      val u = count(map(level)(y-1)(x))
-      val r = count(map(level)(y)(x+1))
-      val d = count(map(level)(y+1)(x))
-
       inner + u + r + d
     } else if(x == 2 && y == 3 && level < 100) {
       val inner = map(level+1)(4).map(count).sum
-      val r = count(map(level)(y)(x+1))
-      val d = count(map(level)(y+1)(x))
-      val l = count(map(level)(y)(x-1))
-
       inner + r + d + l
     } else {
       countNeighbors(map(level), x, y)
@@ -172,11 +132,11 @@ object Day24 extends App {
   }
 
   def countNeighbors(map: Array[String], x: Int, y: Int): Int = {
-    val u = if (y > 0) count(map(y - 1)(x)) else 0
-    val d = if (y < map.length - 1) count(map(y + 1)(x)) else 0
-    val l = if (x > 0) count(map(y)(x - 1)) else 0
-    val r = if (x < map(0).length - 1) count(map(y)(x + 1)) else 0
-
+    val curr = map.map(_.lift).lift
+    val u = count(curr(y-1) flatMap {_(x)} getOrElse '.')
+    val d = count(curr(y+1) flatMap {_(x)} getOrElse '.')
+    val r = count(curr(y) flatMap {_(x+1)} getOrElse '.')
+    val l = count(curr(y) flatMap {_(x-1)} getOrElse '.')
     u + d + r + l
   }
 
@@ -186,5 +146,4 @@ object Day24 extends App {
     else
       0
   }
-
 }
